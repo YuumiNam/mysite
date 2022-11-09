51fe3a9687959f8484915c74bf65f4ea81f499b0
dltjsam <dltjsam@gmail.com>
2022-11-09 11:58:18 +0900

    mysite

---
+++ sql/user.sql
@@ -0,0 +1,11 @@
+desc user;
+
+-- join
+insert into user values(null, '둘리', 'dooli@gmail.com', '1234', 'male', now());
+
+-- select
+select * from user;
+
+-- login
+select no, name from user where email = 'dooli@gmail.com' and password = '1234';
+    
