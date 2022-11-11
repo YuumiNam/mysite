8f6a488138126f6a841ac2ba3c4b5e4c99377250
dltjsam <dltjsam@gmail.com>
2022-11-11 20:28:17 +0900

    mysite

--- sql/board.sql
+++ sql/board.sql
@@ -4,9 +4,13 @@ select * from user;
 
 select * from board;
 
-insert into board values(null, '안녕하세요', '이선무입니다', 0, now(), max(group_no)+1, 1, 0, 1);
+insert into board values(null, '안녕하세요', '이선무입니다', 0, now(), (SELECT max(group_no) + 1), 1, 0, 1);
+
+select max(group_no)+1 from board;
 
 delete from board;
+
+delete from board where no = '1';
 
 select a.no, a.title, b.name, a.hit, date_format(a.date, '%Y/%m/%d %H:%i:%s') as date
 	from board a, user b
