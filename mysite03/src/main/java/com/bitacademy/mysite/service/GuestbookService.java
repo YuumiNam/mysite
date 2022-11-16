38c895245acc6efa575f4b12b4b38cad2ffc9c5e
dltjsam <dltjsam@gmail.com>
2022-11-16 15:26:59 +0900

    mysite

---
+++ mysite03/src/main/java/com/bitacademy/mysite/service/GuestbookService.java
@@ -0,0 +1,21 @@
+package com.bitacademy.mysite.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.bitacademy.mysite.vo.GuestbookVo;
+
+@Service
+public class GuestbookService {
+	
+	public List<GuestbookVo> getContentsList() {
+		return null;
+	}
+	
+	public void addContents(GuestbookVo vo) {
+	}
+	
+	public void deleteContents(Long no, String password) {
+	}
+}
