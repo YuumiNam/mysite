904c9dbc7a35a091d7b55882accf6bf3aaaeda21
dltjsam <dltjsam@gmail.com>
2022-11-17 15:59:23 +0900

    mysite

--- mysite03/src/main/java/com/bitacademy/mysite/repository/UserRepository.java
+++ mysite03/src/main/java/com/bitacademy/mysite/repository/UserRepository.java
@@ -4,9 +4,12 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -18,162 +21,33 @@ public class UserRepository {
 	@Autowired
 	private DataSource dataSource;
 	
+	@Autowired
+	private SqlSession sqlSession;
+	
 	// 로그인
 	public UserVo findByEmailAndPassword(String email, String password) throws UserRepositoryException{
-		UserVo result = null;
+		Map<String, Object> map = new HashMap<>();
+		map.put("email", email);
+		map.put("password", password);
 		
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
+		UserVo result = sqlSession.selectOne("user.findByEmailAndPassword", map);
 		
-		
-		try {
-			// 1, 2
-			conn = dataSource.getConnection();
-
-			// 3. statement 준비
-			String sql = "select no, name from user where email = ? and password = ?"; // 쿼리
-
-			pstmt = conn.prepareStatement(sql); // row값
-
-			// 4. binding
-			pstmt.setString(1, email);
-			pstmt.setString(2, password);
-			
-			// 5. 쿼리 실행
-			rs = pstmt.executeQuery();
-			
-			
-			// 6. result 처리
-			if(rs.next()) {
-				Long no = rs.getLong(1);
-				String name = rs.getString(2);
-				
-				result = new UserVo();
-				result.setNo(no);
-				result.setName(name);
-			}
-
-
-		} catch (SQLException e) {
-			throw new UserRepositoryException(e.toString());
-		} finally {
-			try {
-				if (pstmt != null) {
-					pstmt.close();
-				}
-				if (conn != null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-
 		return result;
 	}
 	
 	// update할 정보 불러오기
 	public UserVo findByNo(Long no) {
-		UserVo result = null;
+		UserVo result = sqlSession.selectOne("user.findByNo", no);
 		
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		
-		
-		try {
-			// 1, 2
-			conn = dataSource.getConnection();
-
-			// 3. statement 준비
-			String sql = "select name, email, gender from user where no = ?"; // 쿼리
-
-			pstmt = conn.prepareStatement(sql); // row값
-
-			// 4. binding
-			pstmt.setLong(1, no);
-			
-			// 5. 쿼리 실행
-			rs = pstmt.executeQuery();
-			
-			
-			// 6. result 처리
-			if(rs.next()) {
-				String name = rs.getString(1);
-				String email = rs.getString(2);
-				String gender = rs.getString(3);
-		
-				result = new UserVo();
-				result.setName(name);
-				result.setEmail(email);
-				result.setGender(gender);
-			}
-
-
-		} catch (SQLException e) {
-			System.out.println("Error:" + e);
-		} finally {
-			try {
-				if (pstmt != null) {
-					pstmt.close();
-				}
-				if (conn != null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-
 		return result;
 	}
 	
 	
 	// 회원가입
 	public Boolean insert(UserVo vo) {
-		boolean result = false;
-		
-		Connection conn = null;
-		PreparedStatement pstmt = null;
+		int count = sqlSession.insert("user.insert", vo);
 
-		try {
-			// 1, 2
-			conn = dataSource.getConnection();
-
-			// 3. statement 준비
-			String sql = "insert into user values(null, ?, ?, ?, ?, now())"; // 쿼리
-
-			pstmt = conn.prepareStatement(sql); // row값
-
-			// 4. binding
-			pstmt.setString(1, vo.getName());
-			pstmt.setString(2, vo.getEmail());
-			pstmt.setString(3, vo.getPassword());
-			pstmt.setString(4, vo.getGender());
-
-			// 5. 쿼리 실행
-			int count = pstmt.executeUpdate(); // executeUpdate()는 insert등은 반영된 건수를 반환, create&drop은 -1을 반환
-
-			// 5. result 처리
-			result = count == 1; // count == 1 << true
-
-		} catch (SQLException e) {
-			System.out.println("Error:" + e);
-		} finally {
-			try {
-				if (pstmt != null) {
-					pstmt.close();
-				}
-				if (conn != null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-
-		return result;
+		return count == 1;
 	}
 
 	
