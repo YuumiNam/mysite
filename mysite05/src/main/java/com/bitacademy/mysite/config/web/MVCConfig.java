3e49efc45ab7153335866fe459341d2454d6c8b6
dltjsam <dltjsam@gmail.com>
2022-12-16 10:00:44 +0900

    message converter

--- mysite05/src/main/java/com/bitacademy/mysite/config/web/MVCConfig.java
+++ mysite05/src/main/java/com/bitacademy/mysite/config/web/MVCConfig.java
@@ -1,7 +1,17 @@
 package com.bitacademy.mysite.config.web;
 
+import java.nio.charset.Charset;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -23,7 +33,44 @@ public class MVCConfig implements WebMvcConfigurer {
 		
 		return viewResolver;
 	}
-
+	
+	// Message Converters
+	@Bean
+	public StringHttpMessageConverter stringHttpMessageConverter() {
+		StringHttpMessageConverter messageConverter = new StringHttpMessageConverter();
+		messageConverter.setSupportedMediaTypes(
+				Arrays.asList(
+					new MediaType("text", "html", Charset.forName("utf-8"))	
+			)
+		);
+		
+		return messageConverter;
+	}
+		
+	@Bean
+	public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+		Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder()
+												.indentOutput(true)
+												.dateFormat(new SimpleDateFormat("YYYY-MM-dd"));
+		
+		MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter(builder.build());
+		
+		messageConverter.setSupportedMediaTypes(
+				Arrays.asList(
+					new MediaType("application", "json", Charset.forName("utf-8"))	
+			)
+		);
+		
+		return messageConverter;
+	}
+		 
+	
+	@Override // <mvc:message-converters>
+	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+		converters.add(stringHttpMessageConverter());
+		converters.add(mappingJackson2HttpMessageConverter());
+	}
+	
 	// MVC Resource Mapping 저장한 파일과 url을 맵핑
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
